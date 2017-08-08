b033bd0846b0fba0454a5097e6fd4ff47e338a60
Raffaele Meyer <raffaele.meyer@students.fhnw.ch>
2017-08-08 16:43:01 +0200

    Update

---
+++ data/data.go
@@ -0,0 +1,73 @@
+package data
+
+import (
+	"encoding/binary"
+	"net"
+)
+
+type IPAddr int32
+type MACAddr int64
+
+type CreateGroupRequest struct {
+	Name              string
+	Password          string
+	NetworkAddr       IPAddr
+	NetworkSubnetMask IPAddr
+}
+
+type CreateGroupResponse struct {
+	OK    bool
+	Error string
+
+	IP      IPAddr
+	Netmask IPAddr
+	Gateway IPAddr
+}
+
+type JoinGroupRequest struct {
+	Name     string
+	Password string
+}
+
+type JoinGroupResponseClient struct {
+	UserName string
+	IP       IPAddr
+}
+
+type JoinGroupResponse struct {
+	OK    bool
+	Error string
+
+	IP      IPAddr
+	Netmask IPAddr
+	Gateway IPAddr
+	Clients []JoinGroupResponseClient
+}
+
+func IntIPtoNetIP(v IPAddr) net.IP {
+	b := make([]byte, 4)
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 24)
+	return b
+}
+func NetIPtoIntIP(b net.IP) IPAddr {
+	return IPAddr(binary.LittleEndian.Uint32(b))
+}
+
+func (v *JoinGroupRequest) Serialize() []byte {
+	return nil
+}
+
+func (v *JoinGroupResponse) Serialize() []byte {
+	return nil
+}
+
+func (v *CreateGroupRequest) Serialize() []byte {
+	return nil
+}
+
+func (v *CreateGroupResponse) Serialize() []byte {
+	return nil
+}
--- server/server.go
+++ server/server.go
@@ -1,11 +1,130 @@
 package server
 
 import (
-	"net"
+	"crypto/subtle"
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/daMupfel/govpn/data"
 )
 
-type CreateVirtualSwitchParameters struct {
+type Instance struct {
+	sync.Mutex
+
+	ActiveGroups map[string]*Group
+}
+
+type Client struct {
+	Name string
+
+	MAC data.MACAddr
+	IP  data.IPAddr
+}
+
+type Group struct {
+	sync.Mutex
 	Name     string
 	Password string
-	Network  net.IPNet
+
+	Net       data.IPAddr
+	Mask      data.IPAddr
+	GatewayIP data.IPAddr
+
+	Clients map[data.MACAddr]*Client
+}
+
+func (i *Instance) JoinGroup(req *data.JoinGroupRequest, c *Client) *data.JoinGroupResponse {
+	i.Lock()
+	defer i.Unlock()
+
+	grp, ok := i.ActiveGroups[req.Name]
+
+	if !ok {
+		return &data.JoinGroupResponse{
+			OK:    false,
+			Error: "Group not found",
+		}
+	}
+
+	grp.Lock()
+	defer grp.Unlock()
+	ip, err := grp.generateNextIP()
+	if err != nil {
+		return &data.JoinGroupResponse{
+			OK:    false,
+			Error: "No free ips",
+		}
+	}
+
+	if subtle.ConstantTimeCompare([]byte(req.Password), []byte(grp.Password)) != 1 {
+		return &JoinGroupResponse{
+			OK:    false,
+			Error: "Invalid password",
+		}
+	}
+
+}
+
+func (i *Instance) CreateGroup(req *data.CreateGroupRequest, c *Client) *data.CreateGroupResponse {
+	i.Lock()
+	defer i.Unlock()
+	name := strings.ToLower(req.Name)
+	if _, ok := i.ActiveGroups[name]; ok {
+		return &data.CreateGroupResponse{
+			OK:    false,
+			Error: "Group already exists",
+		}
+	}
+
+	grp := &Group{
+		Name:     name,
+		Password: req.Password,
+		Net:      req.NetworkAddr,
+		Mask:     req.NetworkSubnetMask,
+		Clients:  make(map[data.MACAddr]*Client),
+	}
+	grp.Clients[c.MAC] = c
+
+	i.ActiveGroups[name] = grp
+	grp.GatewayIP = grp.generateGatewayIP()
+
+	ip, err := grp.generateNextIP()
+	if err != nil {
+		return &data.CreateGroupResponse{
+			OK:    false,
+			Error: "Group contains too little ips",
+		}
+	}
+	return &data.CreateGroupResponse{
+		OK:      true,
+		Error:   "",
+		IP:      ip,
+		Netmask: grp.Mask,
+		Gateway: grp.GatewayIP,
+	}
+}
+
+func (g *Group) generateGatewayIP() data.IPAddr {
+	return g.Net + 0x1000000
+}
+
+func (grp *Group) generateNextIP() (data.IPAddr, error) {
+	grp.Lock()
+	defer grp.Unlock()
+
+	for i := grp.Net + 2; i&grp.Mask == grp.Net&grp.Mask; i++ {
+		found := false
+		for _, c := range grp.Clients {
+			if c.IP == i {
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+		return i, nil
+	}
+	return 0, errors.New("No more ips")
 }
